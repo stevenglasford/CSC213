39cc1874c18a89bc2e3546001755fde8c2055545
Steven Glasford <steven.glasford@ndsu.edu>
2019-09-24 16:13:21 -0500

    Added employees to the list

--- EmplApp1/EmplApp1/Form1.cs
+++ EmplApp1/EmplApp1/Form1.cs
@@ -17,9 +17,25 @@ namespace EmplApp1
             InitializeComponent();
         }
 
+        List<Employee> myEmplList = new List<Employee>();
+
         private void Button1_Click(object sender, EventArgs e)
         {
+            HourlyEmployee myHEmpl = new HourlyEmployee(
+                "aaa", "bb", "123", 25.0M, 50);
+            SalariedEmployee mySalEmpl = new SalariedEmployee(
+                "ddd", "bb", "2222", 40000.0M);
+            CommissionEmployee myComEmpl =
+                new CommissionEmployee("ccc", "cc", "333", 700.0m, 0.5m);
+            BasePlusCommissionEmployee myBasePlusEmpl =
+                new BasePlusCommissionEmployee("www", "ww", "444",
+                700.0m, 0.5m, 80000);
 
+            //add data to the myEmplList
+            myEmplList.Add(myHEmpl);
+            myEmplList.Add(mySalEmpl);
+            myEmplList.Add(myComEmpl);
+            myEmplList.Add(myBasePlusEmpl);
         }
     }
 }
