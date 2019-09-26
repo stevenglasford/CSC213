f386b69b7f9886e330f9df0c39bce062a5d814c9
Steven Glasford <steven.glasford@ndsu.edu>
2019-09-26 15:53:49 -0500

    Accidentally forgot to add my project

---
+++ Glasford_926/Glasford_926/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glasford_926
+{
+    class Account
+    {
+        private decimal balance = 0;
+
+        public Account(decimal initialBalance)
+        {
+            Balance = balance;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value >= 0.0m)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Stupid cunt, you" +
+                        " entered an invalid amount of money!");
+                }
+            }
+        }
+
+    }
+}
