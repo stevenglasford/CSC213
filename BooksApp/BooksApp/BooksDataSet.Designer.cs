83543f1497ae5fc4b45e02a79a5d205893ae50bb
Steven Glasford <stevenglasford@gmail.com>
2019-10-01 16:44:18 -0500

    Added a new query

--- BooksApp/BooksApp/BooksDataSet.Designer.cs
+++ BooksApp/BooksApp/BooksDataSet.Designer.cs
@@ -2016,11 +2016,16 @@ SELECT AuthorID, FirstName, LastName FROM Authors WHERE (AuthorID = @AuthorID)";
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT AuthorID, FirstName, LastName FROM dbo.Authors";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT        AuthorID, FirstName, LastName\r\nFROM            Authors\r\nWHERE      " +
+                "  (LastName LIKE \'D%\')\r\nORDER BY LastName";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -2042,6 +2047,30 @@ SELECT AuthorID, FirstName, LastName FROM Authors WHERE (AuthorID = @AuthorID)";
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual BooksDataSet.AuthorsDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            BooksDataSet.AuthorsDataTable dataTable = new BooksDataSet.AuthorsDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillByLastName(BooksDataSet.AuthorsDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual BooksDataSet.AuthorsDataTable GetDataBy() {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
             BooksDataSet.AuthorsDataTable dataTable = new BooksDataSet.AuthorsDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
