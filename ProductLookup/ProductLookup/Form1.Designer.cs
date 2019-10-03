42a6eab9d237a05bd691e2edfba0cccb416ed2f4
Steven Glasford <stevenglasford@gmail.com>
2019-10-03 16:30:00 -0500

    Finished the first project 11_4

--- ProductLookup/ProductLookup/Form1.Designer.cs
+++ ProductLookup/ProductLookup/Form1.Designer.cs
@@ -28,20 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label descriptionLabel;
+            System.Windows.Forms.Label units_On_HandLabel;
+            System.Windows.Forms.Label priceLabel;
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProductNumberListBox = new System.Windows.Forms.ListBox();
+            this._ProductDB_1_DataSet = new ProductLookup._ProductDB_1_DataSet();
+            this.productDB1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productTableAdapter = new ProductLookup._ProductDB_1_DataSetTableAdapters.ProductTableAdapter();
+            this.productBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tableAdapterManager = new ProductLookup._ProductDB_1_DataSetTableAdapters.TableAdapterManager();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.units_On_HandTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            descriptionLabel = new System.Windows.Forms.Label();
+            units_On_HandLabel = new System.Windows.Forms.Label();
+            priceLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._ProductDB_1_DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDB1DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Product Number";
+            // 
+            // ProductNumberListBox
+            // 
+            this.ProductNumberListBox.DataSource = this.productBindingSource;
+            this.ProductNumberListBox.DisplayMember = "Product_Number";
+            this.ProductNumberListBox.FormattingEnabled = true;
+            this.ProductNumberListBox.Location = new System.Drawing.Point(35, 68);
+            this.ProductNumberListBox.Name = "ProductNumberListBox";
+            this.ProductNumberListBox.Size = new System.Drawing.Size(122, 121);
+            this.ProductNumberListBox.TabIndex = 1;
+            this.ProductNumberListBox.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // _ProductDB_1_DataSet
+            // 
+            this._ProductDB_1_DataSet.DataSetName = "_ProductDB_1_DataSet";
+            this._ProductDB_1_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productDB1DataSetBindingSource
+            // 
+            this.productDB1DataSetBindingSource.DataSource = this._ProductDB_1_DataSet;
+            this.productDB1DataSetBindingSource.Position = 0;
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataMember = "Product";
+            this.productBindingSource.DataSource = this.productDB1DataSetBindingSource;
+            // 
+            // productTableAdapter
+            // 
+            this.productTableAdapter.ClearBeforeFill = true;
+            // 
+            // productBindingSource1
+            // 
+            this.productBindingSource1.DataMember = "Product";
+            this.productBindingSource1.DataSource = this._ProductDB_1_DataSet;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ProductTableAdapter = this.productTableAdapter;
+            this.tableAdapterManager.UpdateOrder = ProductLookup._ProductDB_1_DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // descriptionLabel
+            // 
+            descriptionLabel.AutoSize = true;
+            descriptionLabel.Location = new System.Drawing.Point(214, 78);
+            descriptionLabel.Name = "descriptionLabel";
+            descriptionLabel.Size = new System.Drawing.Size(63, 13);
+            descriptionLabel.TabIndex = 2;
+            descriptionLabel.Text = "Description:";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productBindingSource1, "Description", true));
+            this.descriptionTextBox.Location = new System.Drawing.Point(300, 75);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.descriptionTextBox.TabIndex = 3;
+            // 
+            // units_On_HandLabel
+            // 
+            units_On_HandLabel.AutoSize = true;
+            units_On_HandLabel.Location = new System.Drawing.Point(214, 115);
+            units_On_HandLabel.Name = "units_On_HandLabel";
+            units_On_HandLabel.Size = new System.Drawing.Size(80, 13);
+            units_On_HandLabel.TabIndex = 4;
+            units_On_HandLabel.Text = "Units On Hand:";
+            // 
+            // units_On_HandTextBox
+            // 
+            this.units_On_HandTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productBindingSource1, "Units_On_Hand", true));
+            this.units_On_HandTextBox.Location = new System.Drawing.Point(300, 112);
+            this.units_On_HandTextBox.Name = "units_On_HandTextBox";
+            this.units_On_HandTextBox.Size = new System.Drawing.Size(100, 20);
+            this.units_On_HandTextBox.TabIndex = 5;
+            // 
+            // priceLabel
+            // 
+            priceLabel.AutoSize = true;
+            priceLabel.Location = new System.Drawing.Point(219, 154);
+            priceLabel.Name = "priceLabel";
+            priceLabel.Size = new System.Drawing.Size(34, 13);
+            priceLabel.TabIndex = 6;
+            priceLabel.Text = "Price:";
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productBindingSource1, "Price", true));
+            this.priceTextBox.Location = new System.Drawing.Point(300, 151);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(priceLabel);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(units_On_HandLabel);
+            this.Controls.Add(this.units_On_HandTextBox);
+            this.Controls.Add(descriptionLabel);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.ProductNumberListBox);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Product Lookup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._ProductDB_1_DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDB1DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ProductNumberListBox;
+        private System.Windows.Forms.BindingSource productDB1DataSetBindingSource;
+        private _ProductDB_1_DataSet _ProductDB_1_DataSet;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private _ProductDB_1_DataSetTableAdapters.ProductTableAdapter productTableAdapter;
+        private System.Windows.Forms.BindingSource productBindingSource1;
+        private _ProductDB_1_DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.TextBox units_On_HandTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
     }
 }
 
--- ProductLookup/ProductLookup/Form1.cs
+++ ProductLookup/ProductLookup/Form1.cs
@@ -16,5 +16,17 @@ namespace ProductLookup
         {
             InitializeComponent();
         }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the '_ProductDB_1_DataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this._ProductDB_1_DataSet.Product);
+
+        }
     }
 }
