125478dc3be72d95746e8bf30d3c98a216fd6beb
ndsu.checkout <stevenglasford@gmail.com>
2019-10-03 20:31:26 -0500

    Finished the last Problem from the book that was required 11_8

--- Product Search/Product Search/Form1.cs
+++ Product Search/Product Search/Form1.cs
@@ -16,5 +16,31 @@ namespace Product_Search
         {
             InitializeComponent();
         }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDataSet.Product);
+
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.SearchDesc(
+                this.productDataSet.Product, searchTextBox.Text);
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.Fill(this.productDataSet.Product);
+        }
     }
 }
